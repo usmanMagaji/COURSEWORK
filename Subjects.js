b74d2e9c0752cd23c3496576f3a10b70ef33c1c3
Usman Magaji <usmanabubakarmagaji@yahoo.com>
2021-11-17 18:14:26 +0400

    Added images

---
+++ Subjects.js
@@ -0,0 +1,93 @@
+let products = [
+    {
+      "id": 1001,
+      "Subject": "Math",
+      "Location": "London",
+      "price": 20.00,
+      "image": "./images/math.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1002,
+      "Subject": "English",
+      "Location": "Birmingham",
+      "price": 30.00,
+      "image": "./images/english.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1003,
+      "Subject": "Physics",
+      "Location": "Manchester",
+      "price": 40.00,
+      "image": "./images/cat.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1004,
+      "Subject": "Music",
+      "Location": "Bristol",
+      "price": 50.00,
+      "image": "./images/cat.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1005,
+      "Subject": "French",
+      "Location": "Manchester",
+      "price": 30.00,
+      "image": "./images/cat.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1006,
+      "Subject": "Chemistry",
+      "Location": "London",
+      "price": 20.00,
+      "image": "./images/chemistry.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1007,
+      "Subject": "Arts",
+      "Location": "Birmingham",
+      "price": 10.00,
+      "image": "./images/art.png",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1008,
+      "Subject": "Arabic Language",
+      "Location": "Abuja",
+      "price": 30.00,
+      "image": "./images/cat.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1009,
+      "Subject": "Economics",
+      "Location": "Lagos",
+      "price": 50.00,
+      "image": "./images/cat.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+    {
+      "id": 1010,
+      "Subject": "Biology",
+      "Location": "Leicester",
+      "price": 30.00,
+      "image": "./images/biology.jpg",
+      "availableInventory": 5,
+      "rating": 2
+    },
+
+  ]
